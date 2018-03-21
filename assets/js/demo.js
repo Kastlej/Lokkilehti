2bf51b843b294896efb509205652d75ef777f11a
Jouni Salo <jousa91@hotmail.com>
2018-03-21 17:34:44 +0200

    positioning stuff

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,8 +1,24 @@
-$("#carouselExampleIndicators a").hover(
-  function () {
-    $(this).children("span").addClass("hoverArrow");
-  }, 
-  function () {
-    $(this).children("span").removeClass("hoverArrow");
-  }
-);
+
+
+
+	var scrollLink = $(".scroll");
+
+	//smooth scrollign
+	scrollLink.click(function(e){
+		e.preventDefault();
+		$("body,html").animate({
+			scrollTop: $(this.hash).offset().top
+		});
+	});
+
+
+
+
+	$("#carouselExampleIndicators a").hover(
+	  function () {
+	    $(this).children("span").addClass("hoverArrow");
+	  }, 
+	  function () {
+	    $(this).children("span").removeClass("hoverArrow");
+	  }
+	);
